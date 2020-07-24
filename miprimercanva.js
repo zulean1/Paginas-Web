6d7c146a35d312922b03106b08c9a729b6492de2
zulean1 <zuca0107@gmail.com>
2020-07-23 20:31:09 -0400

    Organizacion de las carpetas

--- miprimercanva.js
+++
@@ -1,8 +0,0 @@
-function iniciar(){
-var elemento=document.getElementById('lienzo');
-lienzo=elemento.getContext('2d');
-lienzo.font="bold 24px verdana, sans-serif";
-lienzo.textAlign="start";
-lienzo.fillText("Mi mensaje", 100,100);
-}
-window.addEventListener("load", iniciar, false);
